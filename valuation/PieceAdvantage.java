227ed87f2a67a204454a5db427e7ce17cfb04402
CSE5705 <therese.smith@uconn.edu>
2012-04-01 01:08:04 -0400

    updated#	../state/BoardWord.java

---
+++ valuation/PieceAdvantage.java
@@ -0,0 +1,8 @@
+package valuation;
+
+public class PieceAdvantage {
+	//p.212 supposed to be a way to calculate this that takes into account that it is advantageous to trade when ahead,
+	//and avoid trades when behind
+	//relative weights of kings to pawns is 3 to 2
+
+}
